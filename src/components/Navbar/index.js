c2deb955148304d8390a8dd221ff0bb5f5a5743f
Sir-Dazzling <ottidan20@gmail.com>
2020-12-08 17:33:45 +0100

    finished navbar

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {Nav, NavIcon, NavLink, Bars} from './NavbarElements';
+
+const Navbar = () => {
+    return (
+         <Nav>
+            <NavLink to="/">Pizza</NavLink>
+            <NavIcon>
+                <p>Menu</p>
+                <Bars />
+            </NavIcon>
+         </Nav>   
+    )
+}
+
+export default Navbar;
